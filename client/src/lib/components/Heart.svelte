86e0c3dc076d0fe6129906d4fdc76ad2a24c5e8b
troyciesco <tmciesco@gmail.com>
2023-01-09 12:02:38 -0500

    heart styles

--- client/src/lib/components/Heart.svelte
+++ client/src/lib/components/Heart.svelte
@@ -20,7 +20,20 @@
   <button
     class="heart-like-button"
     class:liked={favoriteId}
-    type="submit">❤</button>
+    type="submit"
+    ><svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      stroke="currentColor"
+      stroke-width="2"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+      class="feather feather-heart"
+      ><path
+        d="M20.84 4.61a5.5 5.5 0 0 0-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 0 0-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 0 0 0-7.78z" /></svg
+    ></button>
 </form>
 
 <style lang="scss">
@@ -28,14 +41,16 @@
     font-size: 24px;
     background: #ffffff;
     border-radius: 100%;
-    padding: 2px 8px;
+    padding: 4px 6px;
     box-shadow: rgba(0, 0, 0, 0.24) 0px 3px 8px;
-    color: #ffffff;
+    color: #d65076;
+    fill: #ffffff;
     text-shadow: -1px 1px 0 #000, 1px 1px 0 #000, 1px -1px 0 #000, -1px -1px 0 #000;
   }
 
   .heart-like-button.liked {
     color: #d65076;
+    fill: #d65076;
     animation: liked 0.25s ease-in-out;
   }
   @keyframes liked {
